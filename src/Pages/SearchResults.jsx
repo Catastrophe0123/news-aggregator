6ac10285c0304188f7e08885956075606f207c1c
Catastrophe <vnavaneeth15@gmail.com>
2020-08-23 21:51:25 +0530

    added pagination

--- src/Pages/SearchResults.jsx
+++ src/Pages/SearchResults.jsx
@@ -4,17 +4,20 @@ import Axios from 'axios';
 import Stories from '../Components/Stories';
 import Loading from '../Components/Loading';
 
+import { Link } from 'react-router-dom';
+
 export class SearchResults extends Component {
 	state = {
 		articles: [],
 		totalResults: null,
 		loading: true,
 		search: null,
-		showingSource: null,
+		source: null,
+		currentPage: 1,
+		totalPages: null,
 	};
 
-	componentDidMount = async () => {
-		// code
+	getData = async () => {
 		try {
 			// let params = this.props.match.params;
 
@@ -26,56 +29,81 @@ export class SearchResults extends Component {
 			console.log(params);
 			let search = params.get('q');
 			let source = params.get('sources');
-
+			let page = Number(params.get('page'));
+			if (!page) page = 1;
 			// const resp = await Axios.get('/search', { params: { ...params } });
 			console.log('/search' + paramString);
 			const resp = await Axios.get('/search' + paramString);
-
-			if (source) {
-				search = source;
-			}
-
+			let totalPages = Math.ceil(resp.data.totalResults / 20);
 			this.setState({
 				articles: resp.data.articles,
 				totalResults: resp.data.totalResults,
 				loading: false,
 				paramString,
 				search: search,
+				source: source,
 				sourceData: resp.data.sourceData,
+				currentPage: page,
+				totalPages,
 			});
 		} catch (err) {
 			console.log(err.response);
 		}
 	};
 
+	componentDidMount = async () => {
+		await this.getData();
+	};
+
 	componentDidUpdate = async (prevProps, prevState) => {
-		console.log(prevProps);
-		console.log(prevState);
 		let paramString = this.props.location.search;
 		if (prevProps.location.search != paramString) {
-			console.log('/search' + paramString);
 			this.setState({
 				loading: true,
 				paramString: paramString,
 				articles: [],
 			});
-			try {
-				const resp = await Axios.get('/search' + paramString);
-				let params = new URLSearchParams(paramString);
-				let search = params.get('q');
-				this.setState({
-					articles: resp.data.articles,
-					totalResults: resp.data.totalResults,
-					loading: false,
-					search,
-				});
-			} catch (err) {
-				console.log(err.response);
-			}
+
+			await this.getData();
+
+			// try {
+			// 	const resp = await Axios.get('/search' + paramString);
+			// 	let params = new URLSearchParams(paramString);
+			// 	let search = params.get('q');
+			// 	let source = params.get('sources');
+			// 	let currentPage = Number(params.get('page'));
+			// 	if (!currentPage) currentPage = 1;
+			// 	this.setState({
+			// 		articles: resp.data.articles,
+			// 		totalResults: resp.data.totalResults,
+			// 		loading: false,
+			// 		search,
+			// 		source,
+			// 		sourceData: resp.data.sourceData,
+			// 		currentPage,
+			// 	});
+			// } catch (err) {
+			// 	console.log(err.response);
+			// }
 		}
 	};
 
 	render() {
+		let [prevDisabled, nextDisabled] = [false, false];
+		if (this.state.currentPage >= this.state.totalPages)
+			nextDisabled = true;
+		if (this.state.currentPage <= 1) prevDisabled = true;
+		let nextPage = this.state.currentPage + 1;
+		let prevPage = this.state.currentPage - 1;
+		let nextLink, prevLink;
+		if (this.state.source) {
+			nextLink = `/search?sources=${this.state.source}&page=${nextPage}`;
+			prevLink = `/search?sources=${this.state.source}&page=${prevPage}`;
+		} else {
+			nextLink = `/search?q=${this.state.search}&page=${nextPage}`;
+			prevLink = `/search?q=${this.state.search}&page=${prevPage}`;
+		}
+
 		return (
 			<div>
 				{this.state.loading && <Loading />}
@@ -99,6 +127,22 @@ export class SearchResults extends Component {
 						)}
 
 						<Stories articles={this.state.articles} />
+						<div className='flex justify-center '>
+							<button
+								className='border p-5 bg-red-700'
+								disabled={prevDisabled}>
+								<Link to={prevDisabled ? '' : prevLink}>
+									Previous Page
+								</Link>
+							</button>
+							<button
+								className='border p-5 bg-red-700'
+								disabled={nextDisabled}>
+								<Link to={nextDisabled ? '' : nextLink}>
+									Next Page
+								</Link>
+							</button>
+						</div>
 					</div>
 				)}
 			</div>
