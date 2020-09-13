85173bf8cc18d60e0dae8f4eb1f6a5cc7727342d
Catastrophe <vnavaneeth15@gmail.com>
2020-09-13 16:54:12 +0530

    Updates

---
+++ precache-manifest.f73ebdcaa27523b3321c4291a2e790b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28883c0e4d950c6c8b84d195609a7ce2",
+    "url": "/news-aggregator/index.html"
+  },
+  {
+    "revision": "33100d046d9fa0ce04e5",
+    "url": "/news-aggregator/static/css/2.0e6e67bd.chunk.css"
+  },
+  {
+    "revision": "dabc0aa9415dfefaaba5",
+    "url": "/news-aggregator/static/css/main.a9934794.chunk.css"
+  },
+  {
+    "revision": "33100d046d9fa0ce04e5",
+    "url": "/news-aggregator/static/js/2.2568502a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/news-aggregator/static/js/2.2568502a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dabc0aa9415dfefaaba5",
+    "url": "/news-aggregator/static/js/main.470ec3a1.chunk.js"
+  },
+  {
+    "revision": "8492c1da80edd9313d82",
+    "url": "/news-aggregator/static/js/runtime-main.02924361.js"
+  }
+]);
